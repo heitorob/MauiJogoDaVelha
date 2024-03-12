bcb00a1c7e5ceb3cb1daff0d56f9715e00e03f12
Usuário <Usurio@HEITOR>
2024-03-12 19:28:24 -0300

    Jogo da velha que da pra ganhar, empatar e jogar varias vezes.

--- MauiJogoDaVelha/MainPage.xaml.cs
+++ MauiJogoDaVelha/MainPage.xaml.cs
@@ -30,83 +30,149 @@
                btn12.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn20.Text == "X" &&
-                       btn21.Text == "X" &&
-                       btn22.Text == "X")
+                limpar();
+            } 
+            else if (btn20.Text == "X" &&
+                     btn21.Text == "X" &&
+                     btn22.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn30.Text == "X" &&
-                       btn31.Text == "X" &&
-                       btn32.Text == "X")
+                limpar();
+            }
+            else if (btn30.Text == "X" &&
+                     btn31.Text == "X" &&
+                     btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn10.Text == "X" &&
-                       btn20.Text == "X" &&
-                       btn30.Text == "X")
+                limpar();
+            }
+            else if (btn10.Text == "X" &&
+                     btn20.Text == "X" &&
+                     btn30.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn11.Text == "X" &&
-                       btn21.Text == "X" &&
-                       btn31.Text == "X")
+                limpar();
+            }
+            else if (btn11.Text == "X" &&
+                     btn21.Text == "X" &&
+                     btn31.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn12.Text == "X" &&
-                       btn22.Text == "X" &&
-                       btn32.Text == "X")
+                limpar();
+            }
+            else if (btn12.Text == "X" &&
+                     btn22.Text == "X" &&
+                     btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn10.Text == "X" &&
-                       btn21.Text == "X" &&
-                       btn32.Text == "X")
+                limpar();
+            }
+            else if (btn10.Text == "X" &&
+                     btn21.Text == "X" &&
+                     btn32.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn12.Text == "X" &&
-                       btn21.Text == "X" &&
-                       btn30.Text == "X")
+                limpar();
+            }
+            else if (btn12.Text == "X" &&
+                     btn21.Text == "X" &&
+                     btn30.Text == "X")
             {
                 DisplayAlert("Parabéns!", "O X ganhou", "Fechar");
-            } else if (btn10.Text == "O" &&
-                       btn11.Text == "O" &&
-                       btn12.Text == "O")
+                limpar();
+            }
+            else if (btn10.Text == "O" &&
+                     btn11.Text == "O" &&
+                     btn12.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn20.Text == "O" &&
-                       btn21.Text == "O" &&
-                       btn22.Text == "O")
+                limpar();
+            }
+            else if (btn20.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn22.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn30.Text == "O" &&
-                       btn31.Text == "O" &&
-                       btn32.Text == "O")
+                limpar();
+            }
+            else if (btn30.Text == "O" &&
+                     btn31.Text == "O" &&
+                     btn32.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn10.Text == "O" &&
-                       btn20.Text == "O" &&
-                       btn30.Text == "O")
+                limpar();
+            }
+            else if (btn10.Text == "O" &&
+                     btn20.Text == "O" &&
+                     btn30.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn11.Text == "O" &&
-                       btn21.Text == "O" &&
-                       btn31.Text == "O")
+                limpar();
+            }
+            else if (btn11.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn31.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn12.Text == "O" &&
-                       btn22.Text == "O" &&
-                       btn32.Text == "O")
+                limpar();
+            } 
+            else if (btn12.Text == "O" &&
+                     btn22.Text == "O" &&
+                     btn32.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn10.Text == "O" &&
-                       btn21.Text == "O" &&
-                       btn32.Text == "O")
+                limpar();
+            } 
+            else if (btn10.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn32.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
-            } else if (btn12.Text == "O" &&
-                       btn21.Text == "O" &&
-                       btn30.Text == "O")
+                limpar();
+            } 
+            else if (btn12.Text == "O" &&
+                     btn21.Text == "O" &&
+                     btn30.Text == "O")
             {
                 DisplayAlert("Parabéns!", "A O ganhou", "Fechar");
+                limpar();
+            }
+            else if (btn10.Text != "" &&
+                     btn11.Text != "" &&
+                     btn11.Text != "" &&
+                     btn20.Text != "" &&
+                     btn21.Text != "" &&
+                     btn22.Text != "" &&
+                     btn30.Text != "" &&
+                     btn31.Text != "" &&
+                     btn32.Text != "")
+            {
+                DisplayAlert("Empate!", "Deu velha", "Fechar");
+                limpar();
             }
 
+        }
+
+        private void limpar()
+        {
+            btn10.Text = "";
+            btn10.IsEnabled = true;
+            btn11.Text = "";
+            btn11.IsEnabled = true;
+            btn12.Text = "";
+            btn12.IsEnabled = true;
+            btn20.Text = "";
+            btn20.IsEnabled = true;
+            btn21.Text = "";
+            btn21.IsEnabled = true;
+            btn22.Text = "";
+            btn22.IsEnabled = true;
+            btn30.Text = "";
+            btn30.IsEnabled = true;
+            btn31.Text = "";
+            btn31.IsEnabled = true;
+            btn32.Text = "";
+            btn32.IsEnabled = true;
         }
 
     }
